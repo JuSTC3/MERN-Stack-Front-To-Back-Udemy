43c394499b6fff27ce5e587205eb04b4712cdd42
JuSTC3 <thejustc3@gmail.com>
2024-01-23 13:26:34 +0530

    profile

--- DevConnector/routes/api/profile.js
+++ DevConnector/routes/api/profile.js
@@ -1,9 +1,87 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route  GET api/profile
-// @desc   Test route
-// @access Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route  GET api/profile/me
+// @desc   Get current users profile
+// @access Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+
+        if (!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+
+        return res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server Error');
+    }
+});
+
+// @route  POST api/profile
+// @desc   create or update user profile
+// @access Private
+router.get('/', [auth, [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const {
+        website,
+        skills,
+        youtube,
+        twitter,
+        instagram,
+        linkedin,
+        facebook,
+        ...rest
+    } = req.body;
+
+    const profileFields = {
+        user: req.user.id,
+        website: website && website !== ''
+            ? website
+            : '',
+        skills: Array.isArray(skills)
+            ? skills
+            : skills.split(',').map((skill) => ' ' + skill.trim()),
+        ...rest
+    };
+
+    const socialFields = { youtube, twitter, instagram, linkedin, facebook };
+
+    for (const [key, value] of Object.entries(socialFields)) {
+        if (value && value.length > 0)
+            socialFields[key] = value;
+    }
+
+    profileFields.social = socialFields;
+
+    try {
+        // Using upsert option (creates new doc if no match is found):
+
+        let profile = await Profile.findOneAndUpdate(
+            { user: req.user.id },
+            { $set: profileFields },
+            { new: true, upsert: true, setDefaultsOnInsert: true }
+        );
+        return res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server Error');
+    }
+
+});
 
 module.exports = router;
